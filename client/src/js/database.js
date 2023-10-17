8c9ce16cd7024f4ee851b65930c988a2b44f4492
Ashley Zemina <amzemina@gmail.com>
2023-10-17 18:09:42 -0400

    removed TODO comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//  logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Post to the database');
   const jateDB = await openDB('jate', 1);
@@ -23,13 +23,12 @@ export const putDb = async (content) => {
   console.log('Data saved', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+//  logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const jateData = jateDb.transaction('jate', 'readonly');
   const storeData = jateData.objectStore('jate');
-  // const request = storeData.get(1);
   const request = storeData.getAll();
   const result = await request;
   console.log("result.value", result);
